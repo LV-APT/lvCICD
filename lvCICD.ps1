a6bcb5799c347be6447632760a2d3874f74ece21
nevstop <liyao.zheda+github@gmail.com>
2022-03-16 22:58:09 +0800

    test

---
+++ lvCICD.ps1
@@ -0,0 +1,64 @@
+function Get-IniFile
+{
+    param(
+        [parameter(Mandatory = $true)] [string] $filePath
+    )
+
+    $anonymous = "NoSection"
+
+    $ini = @{}
+    switch -regex -file $filePath
+    {
+        "^\[(.+)\]$" # Section
+        {
+            $section = $matches[1]
+            $ini[$section] = @{}
+            $CommentCount = 0
+        }
+
+        "^(;.*)$" # Comment
+        {
+            if (!($section))
+            {
+                $section = $anonymous
+                $ini[$section] = @{}
+            }
+            $value = $matches[1]
+            $CommentCount = $CommentCount + 1
+            $name = "Comment" + $CommentCount
+            $ini[$section][$name] = $value
+        }
+
+        "(.+?)\s*=\s*(.*)" # Key
+        {
+            if (!($section))
+            {
+                $section = $anonymous
+                $ini[$section] = @{}
+            }
+            $name,$value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+
+    return $ini
+}
+
+$cfgPath = "C:\Program Files (x86)\National Instruments\LabVIEW 2019\LabVIEW.ini"
+$fileContent = Get-IniFile $cfgPath
+if ($fileContent["LabVIEW"]["server.tcp.enabled"])
+{
+}
+else{
+	Add-Content $cfgPath "server.tcp.enabled=True"
+}
+
+if($fileContent["LabVIEW"]["server.tcp.port"])
+{
+	$portNum = $fileContent["LabVIEW"]["server.tcp.port"]
+}
+else{
+	$portNum=3363
+}
+
+LabVIEWCLI -OperationName RunVI -VIPath ".\lvapt-CICD.vi" -LabVIEWPath "C:\Program Files (x86)\National Instruments\LabVIEW 2019\LabVIEW.exe" -PortNumber $portNum $args[0] $args[1] $args[2] $args[3] $args[4]
