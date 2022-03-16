ac3968b0b7586d1cfad90a3084d70c84bd295af3
nevstop <liyao.zheda+github@gmail.com>
2022-03-16 22:09:57 +0800

    use pipeline instead

--- ViServerPort.ps1
+++ ViServerPort.ps1
@@ -1,48 +1,48 @@
-function Get-IniFile 
-{  
-    param(  
-        [parameter(Mandatory = $true)] [string] $filePath  
-    )  
-    
+function Get-IniFile
+{
+    param(
+        [parameter(Mandatory = $true)] [string] $filePath
+    )
+
     $anonymous = "NoSection"
-  
-    $ini = @{}  
-    switch -regex -file $filePath  
-    {  
-        "^\[(.+)\]$" # Section  
-        {  
-            $section = $matches[1]  
-            $ini[$section] = @{}  
-            $CommentCount = 0  
-        }  
 
-        "^(;.*)$" # Comment  
-        {  
-            if (!($section))  
-            {  
-                $section = $anonymous  
-                $ini[$section] = @{}  
-            }  
-            $value = $matches[1]  
-            $CommentCount = $CommentCount + 1  
-            $name = "Comment" + $CommentCount  
-            $ini[$section][$name] = $value  
-        }   
+    $ini = @{}
+    switch -regex -file $filePath
+    {
+        "^\[(.+)\]$" # Section
+        {
+            $section = $matches[1]
+            $ini[$section] = @{}
+            $CommentCount = 0
+        }
 
-        "(.+?)\s*=\s*(.*)" # Key  
-        {  
-            if (!($section))  
-            {  
-                $section = $anonymous  
-                $ini[$section] = @{}  
-            }  
-            $name,$value = $matches[1..2]  
-            $ini[$section][$name] = $value  
-        }  
-    }  
+        "^(;.*)$" # Comment
+        {
+            if (!($section))
+            {
+                $section = $anonymous
+                $ini[$section] = @{}
+            }
+            $value = $matches[1]
+            $CommentCount = $CommentCount + 1
+            $name = "Comment" + $CommentCount
+            $ini[$section][$name] = $value
+        }
 
-    return $ini  
-}  
+        "(.+?)\s*=\s*(.*)" # Key
+        {
+            if (!($section))
+            {
+                $section = $anonymous
+                $ini[$section] = @{}
+            }
+            $name,$value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+
+    return $ini
+}
 
 $cfgPath = "C:\Program Files (x86)\National Instruments\LabVIEW 2019\LabVIEW.ini"
 $fileContent = Get-IniFile $cfgPath
@@ -61,4 +61,3 @@ else{
 	$portNum=3363
 }
 echo $portNum
-echo "::set-output name=portNum::$portNum"
