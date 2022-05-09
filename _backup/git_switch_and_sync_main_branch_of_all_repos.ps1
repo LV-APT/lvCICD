dadce6a1605ac7507d4b7c55aaf9af7477517370
NEVSTOP <nevstop@users.noreply.github.com>
2022-05-09 11:08:38 +0800

    update action.yml

--- _backup/git_switch_and_sync_main_branch_of_all_repos.ps1
+++
@@ -1,4 +0,0 @@
-& D:\Workspace\lv-APT-facilities\[danger]git_switch_back_to_main.ps1 skip_pause
-& D:\Workspace\lv-APT-facilities\[danger]git_sync_main_branch_of_all_repos.ps1 skip_pause
-& LabVIEWCLI -OperationName RunVI -VIPath "D:\Workspace\Lv-APT-CI-CD\__TEST-Launcher.vi" -LabVIEWPath "C:\Program Files (x86)\National Instruments\LabVIEW 2019\LabVIEW.exe"
-Pause
