dd583ba5eb83ef0948f27eed41c2b01ca17b2300
nevstop <liyao.zheda+github@gmail.com>
2022-05-30 23:32:53 +0800

    add $OperationVIPath input

--- lvCICD.ps1
+++ lvCICD.ps1
@@ -6,25 +6,29 @@ Write-Host "lvCICD tool script directory is $lvCICD_Tool_dir"
 
 $LabVIEW_Version = $args[0]
 $Architecture = $args[1]
+$OperationVIPath = $args[2]
 if($LabVIEW_Version){} else {$LabVIEW_Version = '2019'}
 if($Architecture){} else {$Architecture = 'x86'}
+if($OperationVIPath){} else {$OperationVIPath = "$lvCICD_Tool_dir"}
+
 
 $LabVIEWExePath = & "$lvCICD_Tool_dir\scripts\LabVIEW_exe_path.ps1" $LabVIEW_Version $Architecture;
 $PortNum = & "$lvCICD_Tool_dir\scripts\ViServerPort.ps1" $LabVIEW_Version;
-$Operation = $args[2]; if($Operation){} else {$Operation = 'lvEcho'}
-$Parameter1 = $args[3]; if ( $Parameter1 ) { $Parameter1 = "'$Parameter1'" }
-$Parameter2 = $args[4]; if ( $Parameter2 ) { $Parameter2 = "'$Parameter2'" }
-$Parameter3 = $args[5]; if ( $Parameter3 ) { $Parameter3 = "'$Parameter3'" }
-$Parameter4 = $args[6]; if ( $Parameter4 ) { $Parameter4 = "'$Parameter4'" }
-$Parameter5 = $args[7]; if ( $Parameter5 ) { $Parameter5 = "'$Parameter5'" }
-$Parameter6 = $args[8]; if ( $Parameter6 ) { $Parameter6 = "'$Parameter6'" }
-$Parameter7 = $args[9]; if ( $Parameter7 ) { $Parameter7 = "'$Parameter7'" }
-$Parameter8 = $args[10]; if ( $Parameter8 ) { $Parameter8 = "'$Parameter8'" }
-$Parameter9 = $args[11]; if ( $Parameter9 ) { $Parameter9 = "'$Parameter9'" }
-$Parameter10 = $args[12]; if ( $Parameter10 ) { $Parameter10 = "'$Parameter10'" }
+$Operation = $args[3]; if($Operation){} else {$Operation = 'lvEcho'}
+$Parameter1 = $args[4]; if ( $Parameter1 ) { $Parameter1 = "'$Parameter1'" }
+$Parameter2 = $args[5]; if ( $Parameter2 ) { $Parameter2 = "'$Parameter2'" }
+$Parameter3 = $args[6]; if ( $Parameter3 ) { $Parameter3 = "'$Parameter3'" }
+$Parameter4 = $args[7]; if ( $Parameter4 ) { $Parameter4 = "'$Parameter4'" }
+$Parameter5 = $args[8]; if ( $Parameter5 ) { $Parameter5 = "'$Parameter5'" }
+$Parameter6 = $args[9]; if ( $Parameter6 ) { $Parameter6 = "'$Parameter6'" }
+$Parameter7 = $args[10]; if ( $Parameter7 ) { $Parameter7 = "'$Parameter7'" }
+$Parameter8 = $args[11]; if ( $Parameter8 ) { $Parameter8 = "'$Parameter8'" }
+$Parameter9 = $args[12]; if ( $Parameter9 ) { $Parameter9 = "'$Parameter9'" }
+$Parameter10 = $args[13]; if ( $Parameter10 ) { $Parameter10 = "'$Parameter10'" }
 
 Write-Host "LabVIEW_Version = $LabVIEW_Version"
 Write-Host "Architecture = $Architecture"
+Write-Host "OperationVIPath = $OperationVIPath"
 Write-Host "Operation = $Operation"
 Write-Host "Parameter1 = $Parameter1"
 Write-Host "Parameter2 = $Parameter2"
@@ -48,7 +52,7 @@ Start-Sleep -Seconds 10
 
 $lvCICDVIPath = "$lvCICD_Tool_dir\LabVIEW-Adapter\lvCICD.vi"
 
-Write-Host "LabVIEWCLI -OperationName RunVI -VIPath ""$lvCICDVIPath"" -LogFilePath ""$lvCICD_Tool_dir\lVCLI.log"" -LogToConsole True -LabVIEWPath ""$LabVIEWExePath"" -PortNumber $PortNum $Operation $Parameter1 $Parameter2 $Parameter3 $Parameter4 $Parameter5"
+Write-Host "LabVIEWCLI -OperationName RunVI -VIPath ""$lvCICDVIPath"" -LogFilePath ""$lvCICD_Tool_dir\lVCLI.log"" -LogToConsole True -LabVIEWPath ""$LabVIEWExePath"" -PortNumber $PortNum ""$OperationVIPath"" $Operation $Parameter1 $Parameter2 $Parameter3 $Parameter4 $Parameter5"
 
 LabVIEWCLI -OperationName RunVI `
     -VIPath "$lvCICDVIPath" `
@@ -56,6 +60,7 @@ LabVIEWCLI -OperationName RunVI `
     -LogToConsole True `
     -LabVIEWPath "$LabVIEWExePath" `
     -PortNumber $PortNum `
+    "$OperationVIPath" `
     $Operation `
     $Parameter1 `
     $Parameter2 `
